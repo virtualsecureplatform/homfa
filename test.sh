fac32e7caa523711ef5f1581bcec2cecd495a5d4
Ushitora Anqou <ushitora@anqou.net>
2021-05-19 11:34:46 +0900

    Rename test/03* to test/01*

--- test.sh
+++ test.sh
@@ -18,9 +18,9 @@ check_false(){
     [ $? -eq 0 ] || failwith "Expected false, got true >>> $HOMFA \"$1\" \"$2\""
 }
 
-check_true  test/03.spec test/03-01.in # [1, 1] * 8 * 100
-check_false test/03.spec test/03-02.in # [1, 0] * 8 * 100
-check_true  test/03.spec test/03-03.in # [0, 0, 1, 0, 0, 1, 0, 1, 1, 1] * 8 * 20
-check_false test/03.spec test/03-04.in # [1, 0] + ([0, 0, 1, 0] * 8 * 20) + [0, 1] + ([0, 0, 0, 1] * 8 * 20)
-check_true  test/03.spec test/03-05.in # [0, 0, 1, 0, 0, 1, 0, 1, 1, 1] * 8 * 100
-check_false test/03.spec test/03-06.in # [1, 0] + ([0, 0, 1, 0] * 8 * 100) + [0, 1] + ([0, 0, 0, 1] * 8 * 150)
+check_true  test/01.spec test/01-01.in # [1, 1] * 8 * 100
+check_false test/01.spec test/01-02.in # [1, 0] * 8 * 100
+check_true  test/01.spec test/01-03.in # [0, 0, 1, 0, 0, 1, 0, 1, 1, 1] * 8 * 20
+check_false test/01.spec test/01-04.in # [1, 0] + ([0, 0, 1, 0] * 8 * 20) + [0, 1] + ([0, 0, 0, 1] * 8 * 20)
+check_true  test/01.spec test/01-05.in # [0, 0, 1, 0, 0, 1, 0, 1, 1, 1] * 8 * 100
+check_false test/01.spec test/01-06.in # [1, 0] + ([0, 0, 1, 0] * 8 * 100) + [0, 1] + ([0, 0, 0, 1] * 8 * 150)
