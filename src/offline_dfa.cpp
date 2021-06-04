c13592e4538af774f56a73eeac7e188fea7cbe41
Ushitora Anqou <ushitora@anqou.net>
2021-06-04 15:04:58 +0900

    Prepare next run in parallel

--- src/offline_dfa.cpp
+++ src/offline_dfa.cpp
@@ -4,6 +4,7 @@
 #include <queue>
 
 #include <spdlog/spdlog.h>
+#include <ThreadPool.h>
 
 /* OfflineDFARunner */
 
@@ -531,14 +532,16 @@ void GPUOfflineDFARunner::eval()
 {
     assert(!has_evaluated_);
 
+    ThreadPool pool(1);
     auto run0 = std::make_shared<CUDARunner>(3, graph_);
     auto run1 = std::make_shared<CUDARunner>(3, graph_);
     run0->make_ouroboros(*run1);
     run0->set_initial_weight();
     run0->prepare_next_run(input_stream_);
     while (input_stream_.size() != 0) {
-        run0->run();
+        auto f = pool.enqueue([&run0]{run0->run();});
         run1->prepare_next_run(input_stream_);
+        f.wait();
 
         using std::swap;
         swap(run0, run1);
