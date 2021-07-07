676917206a1a13dcfab5fd3d916ada1d0391ae4c
Ushitora Anqou <ushitora@anqou.net>
2021-07-07 14:57:57 +0900

    [online-reversed] Do minimization of reversed DFA

--- src/online_dfa.cpp
+++ src/online_dfa.cpp
@@ -87,7 +87,7 @@ void OnlineDFARunner::bootstrap_weight()
 /* OnlineDFARunner2 */
 OnlineDFARunner2::OnlineDFARunner2(const Graph &graph,
                                    std::shared_ptr<GateKey> gate_key)
-    : runner_(graph.reversed(), std::nullopt, gate_key)
+    : runner_(graph.reversed().minimized(), std::nullopt, gate_key)
 {
 }
 
